78f0f6007bac90221ee53baff43014f915978867
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-10-22 17:09:42 +0700

    task3 function returns list

--- solution3.py
+++ solution3.py
@@ -1,12 +1,12 @@
 strs = [str(s) for s in input().split('|')]
 
-def matrix(y, a, b):
+def matrix(y):
   m = []
   for i in range(len(y)):
     m.append([float(s) for s in y[i].split()])
-  return m[a][b]
+  return m
 
-assert matrix("1 2 | 3 4".split('|'), 0, 1) == 2.0
-assert matrix("34 64 34 | 45 28 5 | 3 78 9".split('|'), 2, 2) == 9.0
+assert matrix("1 2 | 3 4".split('|'))[0][1] == 2.0
+assert matrix("34 64 34 | 45 28 5 | 3 78 9".split('|'))[2][2] == 9.0
 
-print(matrix(strs, 0, 1))
+print(matrix(strs)[0][1])
