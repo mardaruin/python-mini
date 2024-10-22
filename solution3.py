cac118e93baf412e40be706318fe526b53735fe1
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-10-22 17:21:56 +0700

    task3 tests compare lists

--- solution3.py
+++ solution3.py
@@ -6,7 +6,7 @@ def matrix(y):
     m.append([float(s) for s in y[i].split()])
   return m
 
-assert matrix("1 2 | 3 4".split('|'))[0][1] == 2.0
-assert matrix("34 64 34 | 45 28 5 | 3 78 9".split('|'))[2][1] == 78.0
+assert matrix("1 2 | 3 4".split('|')) == [[1.0, 2.0], [3.0, 4.0]]
+assert matrix("34 64 34 | 45 28 5 | 3 78 9".split('|')) == [[34.0, 64.0, 34.0], [45.0, 28.0, 5.0], [3.0, 78.0, 9.0]]
 
-print(matrix(strs)[0][1])
+print(matrix(strs))
