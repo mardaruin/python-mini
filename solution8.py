b4a0c4135ade3b4725136eda74b047123b6e1146
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-12-04 17:32:18 +0700

    task8 fixed

--- solution8.py
+++ solution8.py
@@ -1,6 +1,10 @@
 
 def format_table(benchmarks, algos, results):
-    col_size = max(max(map(len, [f"{bench}" for bench in benchmarks])), max(map(len, [f"{alg}" for alg in algos])), len("Benchmark"))
+    all_values = [f"{bench}" for bench in benchmarks]
+    all_values.append('Benchmark')
+    all_values.extend([f"{alg}" for alg in algos])
+    all_values.extend([str(value) for row in results for value in row])
+    col_size = max(map(len, all_values))
     separator = "-" * ((col_size + 2) * (len(algos) + 1) + len(algos) + 2)
     header = f"| " + f"{'Benchmark':<{col_size}}" + f" | " + " | ".join(f"{alg:<{col_size}}" for alg in algos) + " |"
     #f"| {" ".join(f"{name:<{col_size}}" for name in ["Benchmark"] + algos)} |"
@@ -12,6 +16,6 @@ def format_table(benchmarks, algos, results):
         print(string)
 
 if __name__ == '__main__':
-    print (format_table(["best case", "worst case"],["quick sort", "merge sort", "bubble sort"],[[1.23, 1.56, 2.0], [3.3, 2.9, 3.9]]))
+    print (format_table(["best", "worst"],["quick sort", "merge sort", "bubble sort"],[[1.23, 1.56, 2.0], [3.3, 2.9, 3.9]]))
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-    print(format_table(["3", "4"], ["2", "5", "7"], [["6", "15", "21"], ["8", "20", "28"]]))
+    print(format_table(["3", "4"], ["2", "5", "7"], [[122, 10 ** 11, 2.0], [3.3, 2.9, 3.9]]))
