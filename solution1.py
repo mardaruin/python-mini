7c570114da04899d5ea735fcaab910dac125a472
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-12-05 20:03:27 +0700

    task1 fixed

--- solution1.py
+++ solution1.py
@@ -1,26 +1,26 @@
-a = int(input())
-count = 0
-numberofbits = 1
 
-if a > 0:
-    while a > 0:
-        count += a % 2
-        a //= 2
-else:
-    a *= -1
-    count = 1
-    if a % 2 == 0:
-        while a % 2 == 0:
-            numberofbits += 1
-            a //= 2
-        count += 1
-    else: count += 1
-    while a > 0:
-        numberofbits += 1
-        count += (a + 1) % 2
-        a //= 2
-    if numberofbits % 8 != 0:
-        count += 8 - numberofbits % 8
+def number_of_bits(number):
+    bit_length = 0
+    temp_number = abs(number)
+    while temp_number != 0:
+        bit_length += 1
+        temp_number >>= 1
 
+    ones_count = 0
+    if number < 0:
+        number = ~abs(number) + 1
+        ones_count += 1
 
-print(count)
+    for _ in range(bit_length):
+        ones_count += number & 1
+        number >>= 1
+
+    return ones_count
+
+assert(number_of_bits(-1)) == 2
+assert(number_of_bits(-12345678)) == 13
+assert(number_of_bits(12345678)) == 12
+assert(number_of_bits(-123)) == 3
+assert(number_of_bits(1)) == 1
+
+print(number_of_bits(-556))
