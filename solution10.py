928a7a7f2b6d88a5f24a589231dc8364073d1451
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-12-25 00:22:12 +0700

    task10 fixed

--- solution10.py
+++ solution10.py
@@ -4,26 +4,44 @@ from dataclasses import dataclass
 class Counter:
     count: int = 0
     step: int = 1
+    some: int = 2
 
     def increment(self):
         self.count += self.step
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 class Singleton:
     _instances = None
+
+    def no_function(self, *args, **kwargs):
+        print("in no_function")
+        pass
 
     def __new__(cls, *args, **kwargs):
         if cls._instances is None:
             cls._instances = super().__new__(cls)
-            cls._instances.__init__(*args, **kwargs)
+            print("in __new__, instance created ")
+            #cls._instances.__init__(*args, **kwargs)
+        cls.__init__ = cls.no_function
+        print("in __new__, will return cls._instances ")
         return cls._instances
 
+
     def __init__(self, *args, **kwargs):
+        print("in __init__ ")
         pass
 
 class GlobalCounter(Singleton, Counter):
     pass
 
+print("create first GlobalCounter ")
 gc1 = GlobalCounter()
+print("create second GlobalCounter ")
 gc2 = GlobalCounter()
 
 assert id(gc1) == id(gc2)
+
+gc1.increment()
+assert gc2.count == 1
