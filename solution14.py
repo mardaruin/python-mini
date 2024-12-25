bbce972c2ece65511cdaaa33f0b25ddd16af3ab4
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-12-25 21:04:09 +0700

    task14 fixed

--- solution14.py
+++ solution14.py
@@ -47,51 +47,41 @@ def simulate(grid, iterations):
 def create_grid_np(size):
     return np.zeros((size, size), dtype=np.uint8)
 
-def get_neighbor_sum(padded_grid, method):
-    if method == 'indexing':
-        return (
-            padded_grid[:-2, :-2] +
-            padded_grid[:-2, 1:-1] +
-            padded_grid[:-2, 2:] +
-            padded_grid[1:-1, :-2] +
-            padded_grid[1:-1, 2:] +
-            padded_grid[2:, :-2] +
-            padded_grid[2:, 1:-1] +
-            padded_grid[2:, 2:]
-        )
-    elif method == 'item':
-        return (
-            padded_grid[:-2, :-2].sum() +
-            padded_grid[:-2, 1:-1].sum() +
-            padded_grid[:-2, 2:].sum() +
-            padded_grid[1:-1, :-2].sum() +
-            padded_grid[1:-1, 2:].sum() +
-            padded_grid[2:, :-2].sum() +
-            padded_grid[2:, 1:-1].sum() +
-            padded_grid[2:, 2:].sum()
-        )
-    elif method == 'flat':
-        return (
-            padded_grid[:-2, :-2].flat[0] +
-            padded_grid[:-2, 1:-1].flat[0] +
-            padded_grid[:-2, 2:].flat[0] +
-            padded_grid[1:-1, :-2].flat[0] +
-            padded_grid[1:-1, 2:].flat[0] +
-            padded_grid[2:, :-2].flat[0] +
-            padded_grid[2:, 1:-1].flat[0] +
-            padded_grid[2:, 2:].flat[0]
-        )
+def get_neighbor_sum(grid, x, y, method):
+    neighbors = 0
+
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if (i == x and j == y) or not (0 <= i < len(grid)) or not (0 <= j < len(grid)):
+                continue
+            if method == 'indexing':
+                neighbors += grid[i, j]
+            elif method == 'item':
+                neighbors += grid.item((i, j))
+    return neighbors
 
 def update_grid_np(grid, method):
     new_grid = np.copy(grid)
-
-    padded_grid = np.pad(grid, 1, mode='constant', constant_values=0)
-    neighbors_count = get_neighbor_sum(padded_grid, method)
-
-    new_grid[(grid == 0) & (neighbors_count == 3)] = 1
-    new_grid[(grid == 1) & ((neighbors_count < 2) | (neighbors_count > 3))] = 0
-
+    size = len(grid)
+    for i in range(size):
+        for j in range(size):
+            neighbors = get_neighbor_sum(grid, i, j, method)
+            if method == 'indexing':
+                if grid[i, j]:
+                    if neighbors < 2 or neighbors > 3:
+                        new_grid[i, j] = 0
+                else:
+                    if neighbors == 3:
+                        new_grid[i, j] = 1
+            elif method == 'item':
+                if grid.item((i, j)):
+                    if neighbors < 2 or neighbors > 3:
+                        new_grid.itemset((i, j), 0)
+                else:
+                    if neighbors == 3:
+                        new_grid.itemset((i, j), 1)
     return new_grid
+
 
 
 def simulate_np(grid, iterations, method):
@@ -118,9 +108,3 @@ initial_grid_np = np.array(initial_grid)
 simulate(initial_grid, ITERATIONS)
 simulate_np(initial_grid_np, ITERATIONS, 'indexing')
 simulate_np(initial_grid_np, ITERATIONS, 'item')
-simulate_np(initial_grid_np, ITERATIONS, 'flat')
-
-#Simulation with list took 49.052925 seconds
-#Simulation with NumPy (indexing) took 0.357805 seconds
-#Simulation with NumPy (item) took 0.154006 seconds
-#Simulation with NumPy (flat) took 0.169240 seconds
